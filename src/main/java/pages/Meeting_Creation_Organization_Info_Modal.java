c88f2c491849af00b331170dfab54c4324ea425a
volodymyr.zinchenko <volodymyr.zinchenko@ctdev.io>
2020-02-26 17:18:27 +0200

    1

--- src/main/java/pages/Meeting_Creation_Organization_Info_Modal.java
+++ src/main/java/pages/Meeting_Creation_Organization_Info_Modal.java
@@ -10,10 +10,12 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class Meeting_Creation_Organization_Info_Modal {
 
-    @FindBy(xpath = "(.//*[(.)='Organizing Country'])[1]/preceding::input[1]")
+    @FindBy(xpath = "(.//*[(.)=\"Organizing Business Unit\"])[1]/following::input[1]")
     public WebElement organizing_business_unit_field;
-    @FindBy(xpath = "//lightning-base-combobox-item[(text() = 'engoce_Party__cTesting' or . = 'engoce_Party__cTesting')]")
+    @FindBy(xpath = "//lightning-base-combobox-item[(text() = \"engoce_Party__cTesting\" or . = \"engoce_Party__cTesting\")]")
     public WebElement business_unit_item;
+    @FindBy(xpath = "(.//*[text()=\"No\"])[2]/preceding::span[1]")
+    public WebElement hosting_country_radio_yes;
 
     public Meeting_Creation_Organization_Info_Modal(WebDriver driver) throws Exception {
         PageFactory.initElements(driver, this);
@@ -21,6 +23,11 @@ public class Meeting_Creation_Organization_Info_Modal {
 
     public Meeting_Creation_Organization_Info_Modal organizing_business_unit_click() throws Exception {
         $(organizing_business_unit_field).waitUntil(visible,10000).click();
+        return this;
+    }
+
+    public Meeting_Creation_Organization_Info_Modal hosting_country_radio_yes_click() throws Exception {
+        $(hosting_country_radio_yes).waitUntil(visible,10000).click();
         return this;
     }
 
